3b66781af231cba80f8a46e558baeb552118721b
vijayalakshmi puligedda <vijayalakshmipuligedda2@gmail.com>
2025-12-21 20:45:35 +0530

    Added address alignment constraint

--- axi_txn.sv
+++ axi_txn.sv
@@ -4,6 +4,10 @@ class axi_txn extends uvm_sequence_item;
   rand logic [31:0] data;
   rand logic        write;
 
+  constraint addr_align {
+    addr[1:0] == 2'b00;
+  }
+
   `uvm_object_utils(axi_txn)
 
   function new(string name = "axi_txn");
