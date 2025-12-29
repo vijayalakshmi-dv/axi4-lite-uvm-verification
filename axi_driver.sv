266b52453faeb683367e94a34a5c23d5124d6852
vijayalakshmi puligedda <vijayalakshmipuligedda2@gmail.com>
2025-12-29 09:02:15 +0530

    Connected virtual interface to AXI driver

--- axi_driver.sv
+++ axi_driver.sv
@@ -1,9 +1,19 @@
 class axi_driver extends uvm_driver #(axi_txn);
 
+  // virtual interface handle
+  virtual axi_if vif;
+
   `uvm_component_utils(axi_driver)
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    if (!uvm_config_db #(virtual axi_if)::get(this, "", "vif", vif))
+      `uvm_fatal("DRV", "Virtual interface not set")
   endfunction
 
 endclass
