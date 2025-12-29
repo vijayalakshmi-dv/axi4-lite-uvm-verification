40d4ab989e9f218f0ee3877f5b6278d7cc020b33
vijayalakshmi puligedda <vijayalakshmipuligedda2@gmail.com>
2025-12-29 09:46:57 +0530

    Started AXI sequence from test

--- axi_test.sv
+++ axi_test.sv
@@ -1,6 +1,7 @@
 class axi_test extends uvm_test;
 
   axi_env env;
+  axi_seq seq;
 
   `uvm_component_utils(axi_test)
 
@@ -13,5 +14,14 @@ class axi_test extends uvm_test;
 
     env = axi_env::type_id::create("env", this);
   endfunction
+
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+
+    seq = axi_seq::type_id::create("seq");
+    seq.start(env.drv.seq_item_port);
+
+    phase.drop_objection(this);
+  endtask
 
 endclass
