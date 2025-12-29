cfeab090a91844a07c09ad3810a291b0dde08e23
vijayalakshmi puligedda <vijayalakshmipuligedda2@gmail.com>
2025-12-29 09:21:55 +0530

    Added run phase to AXI monitor to observe awvalid

--- axi_monitor.sv
+++ axi_monitor.sv
@@ -16,4 +16,17 @@ class axi_monitor extends uvm_monitor;
       `uvm_fatal("MON", "Virtual interface not set")
   endfunction
 
+  task run_phase(uvm_phase phase);
+    // wait for reset deassertion
+    wait (vif.reset_n == 1);
+
+    forever begin
+      @(posedge vif.clk);
+
+      // observe ONE signal
+      if (vif.awvalid)
+        `uvm_info("MON", "AWVALID observed HIGH", UVM_LOW)
+    end
+  endtask
+
 endclass
