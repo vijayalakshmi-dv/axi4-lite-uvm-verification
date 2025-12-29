51f7fff6693d08c5f6f3c6015ddbadcc18349454
vijayalakshmi puligedda <vijayalakshmipuligedda2@gmail.com>
2025-12-29 09:06:33 +0530

    Synchronized AXI driver with clock

--- axi_driver.sv
+++ axi_driver.sv
@@ -1,6 +1,5 @@
 class axi_driver extends uvm_driver #(axi_txn);
 
-  // virtual interface handle
   virtual axi_if vif;
 
   `uvm_component_utils(axi_driver)
@@ -15,5 +14,21 @@ class axi_driver extends uvm_driver #(axi_txn);
     if (!uvm_config_db #(virtual axi_if)::get(this, "", "vif", vif))
       `uvm_fatal("DRV", "Virtual interface not set")
   endfunction
+
+ task run_phase(uvm_phase phase);
+  axi_txn tr;
+
+  forever begin
+    seq_item_port.get_next_item(tr);
+
+    // wait for clock edge
+    @(posedge vif.clk);
+
+    // drive one signal
+    vif.awvalid <= tr.write;
+
+    seq_item_port.item_done();
+  end
+endtask
 
 endclass
